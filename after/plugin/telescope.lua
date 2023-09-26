40d955f05c2902f4848bbdf004757a6dcd6f0cb2
Vladislav Dedovich <vladislav.dedovich@gmail.com>
2023-09-26 14:59:46 +0000

    add null-ls

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,10 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+
+telescope.load_extension("fzf")
+    
+vim.keymap.set('n', '<leader>ff', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>pr', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function() 
 	builtin.grep_string({ search = vim.fn.input("Grep >") });
--- lua/mificot/packer.lua
+++ lua/mificot/packer.lua
@@ -1,53 +1,60 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  -- fuzzy-finder
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  -- theme
-  use 'AlexvZyl/nordic.nvim'
-
-  -- treesitter
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-
-  -- harpoon
-  use('theprimeagen/harpoon')
-
-  -- fugitive
-  use('tpope/vim-fugitive')
-
-  -- lsp-zero
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {'williamboman/mason.nvim'},           -- Optional
-		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},     -- Required
-		  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-		  {'L3MON4D3/LuaSnip'},     -- Required
-	  }
-  }
-
-  use {
-    'folke/trouble.nvim',
-    requires = {
-        'nvim-tree/nvim-web-devicons'
+    -- fuzzy-finder
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        requires = { { 'nvim-lua/plenary.nvim' }, { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+            { 'nvim-tree/nvim-web-devicons' } }
     }
-  }
 
-  use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
+    -- theme
+    use 'AlexvZyl/nordic.nvim'
+
+    -- treesitter
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+    -- harpoon
+    use('theprimeagen/harpoon')
+
+    -- fugitive
+    use('tpope/vim-fugitive')
+
+    use('lewis6991/gitsigns.nvim')
+
+    -- lsp-zero
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
+    }
+
+    use {
+        'jose-elias-alvarez/null-ls.nvim'
+    }
+
+    use {
+        'folke/trouble.nvim',
+        requires = {
+            'nvim-tree/nvim-web-devicons'
+        }
+    }
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)
